0062458fd0f248dbf8393b53258b357190ecfbb5
DmitryKotusev <dssa-1966@yandex.ru>
2019-12-21 21:10:51 +0300

    Progress on UI.

--- yacht-service/src/app/app.module.ts
+++ yacht-service/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HeaderModule } from './header';
+import { MainPageModule } from './main-page';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    HeaderModule
+    HeaderModule,
+    MainPageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- yacht-service/src/app/header/header.component.ts
+++ yacht-service/src/app/header/header.component.ts
@@ -12,4 +12,23 @@ export class HeaderComponent implements OnInit {
   ngOnInit() {
   }
 
+  onToggleButtonPress(navbar: any): void {
+    var classList : DOMTokenList = navbar.classList;
+
+    if (classList.contains("collapse")) {
+      classList.remove("collapse");
+    }
+    else {
+      classList.add("collapse");
+    }
+  }
+
+  onDropDownPress(dropDownContent: any): void {
+    if (dropDownContent.style.display === '') {
+      dropDownContent.style.display = 'none';
+    }
+    else {
+      dropDownContent.style.display = '';
+    }
+  }
 }
